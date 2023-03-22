af4ea5c827467a1b3ab92e63df85f2d057c59a1d
Samuel Dornelas de Oliveira <Zdornelas2@academicos.com>
2023-03-22 19:32:33 -0300

    atualização

---
+++ Ted4 Questão01.py
@@ -0,0 +1,7 @@
+Num01 = float(input('Digite um numero:'))
+
+if Num01 >= 0:
+    print('Positivo')
+
+else:
+    print('Negativo')
---
+++ Ted4 Questão02.py
@@ -0,0 +1,7 @@
+num02 = int(input('Digite o numeros de maças:'))
+
+if num02 >= 12:
+    print (num02 * 1.0)
+
+else:
+    print(num02 * 1.30)
