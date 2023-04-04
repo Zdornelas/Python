81991ad937d815e8958831f529cd4e4878cc04ed
Zdornelas2 <samueldornelas.pb@gmail.com>
2023-04-03 21:25:26 -0300

    atualização

---
+++ aula05.py
@@ -0,0 +1,26 @@
+#Aula05 - While/For
+
+
+#Atividade 01
+spam = 0
+while spam < 5:
+    print('Hello world')
+    spam = spam +1
+
+#Atividade 02
+print('my name is')
+for i in range(10):
+    print(f'jimmy fives times({i})')
+
+
+#Ativadade 03
+num = 1
+while num <11:
+    print(num)
+    num = num +1
+
+
+#Ativadade 04
+num = 1
+for num in range (11):
+    print(num)
